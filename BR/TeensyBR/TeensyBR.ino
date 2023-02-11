8cddd8d8c91e3b06b960cff52eea12b0dfc028a2
Etienne <etienne.fouque2000@gmail.com>
2023-02-11 01:13:53 +0100

    First files of the new C code

---
+++ BR/TeensyBR/TeensyBR.ino
@@ -0,0 +1,40 @@
+/*
+     ____                                                  
+    / ___| _   _ _ __   __ _  ___ _ __ ___                 
+    \___ \| | | | '_ \ / _` |/ _ \ '__/ _ \                
+     ___) | |_| | |_) | (_| |  __/ | | (_) |               
+    |____/ \__,_| .__/ \__,_|\___|_|  \___/                
+   ____       _ |_|       _   _ _       ____ _       _     
+  |  _ \ ___ | |__   ___ | |_(_) | __  / ___| |_   _| |__  
+  | |_) / _ \| '_ \ / _ \| __| | |/ / | |   | | | | | '_ \ 
+  |  _ < (_) | |_) | (_) | |_| |   <  | |___| | |_| | |_) |
+  |_| \_\___/|_.__/ \___/ \__|_|_|\_\  \____|_|\__,_|_.__/ 
+
+
+This code is to be uploaded on the TeensyBR
+It is full C, with a cyclic call to functions at a given period
+
+
+**** DOC ****
+
+Penser une structure en blocs de calcul qui se suivent (comme Matlab) -> peuvent prendre en entrée la sorte du bloc suivant
+- Struct contexte (vars intermédiaires et params)
+- Struct entrée
+- Struct sortie"
+
+Les fonctions utilisent les strucuture de cette manière :
+fct(entree, contexte, *sortie)
+
+il faut faire les opérations sur les blocs dans le bon ordre mais y a pas de concurrence de tâches
+→ revient à faire un scéma simulink
+
+Follow the carrot pour l’asserv pos
+Un point de contrôle pas au centre du robot qui cherche à aller sur un point visé (qui se déplace à la vitesse de consigne)
+Pour l’instant le point visé fait des trajectoires simples (rot lin rot)
+→ mail mathieu qui a de la doc
+
+*************
+
+*/
+
+// The setup and loop are in the setup_loop files
