709d270f151d51067c49edf2964211314387d124
Etienne <etienne.fouque2000@gmail.com>
2023-04-08 19:15:26 +0200

    1st rough odos calibration

--- BR/TeensyBRpio/include/OdosPosition.hpp
+++ BR/TeensyBRpio/include/OdosPosition.hpp
@@ -25,9 +25,9 @@ public :
 
 
     // le L impose les constantes en double precisions (pour une teensy)
-    static constexpr float ECARTS_ODOS = 1035.095823L; //1036.02140234218L; //1026.58121393134L;     // (ticks.rad^(-1) ecart entre les 2 odos
-    static constexpr float UNITS_ODOS = 12.74291667L; //12.756L;    // ticks.mm^(-1)
-    static constexpr float L_R_ODOS = 1.000667458L; //1.00133324111081L; //0.999399650554833L ;  // rapport des rapports... (var en R et L)
+    static constexpr float ECARTS_ODOS = 5964.529501170628L;  // (ticks.rad^(-1) ecart entre les 2 odos
+    static constexpr float UNITS_ODOS = 51.50592885375494L;  // ticks.mm^(-1)
+    static constexpr float L_R_ODOS = 0.9955054176905741L;  // rapport des rapports... (var en R et L)
 private :
 
     int32_t m_odoLeftCount = 0;
